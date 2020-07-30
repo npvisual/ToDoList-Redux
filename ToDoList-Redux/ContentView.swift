921c29857b4590ba4d3bd83f59b5d6e792d80289
Nick <npvisual@users.noreply.github.com>
2020-07-30 18:04:08 -0500

    Initial Commit

---
+++ ToDoList-Redux/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ToDoList-Redux
+//
+//  Created by Nicolas Philippe on 7/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoList-Redux/ToDoList_ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_ReduxApp.swift
+//  ToDoList-Redux
+//
+//  Created by Nicolas Philippe on 7/30/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
